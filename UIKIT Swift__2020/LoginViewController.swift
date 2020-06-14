7f795f600582039296aa64b1dc072bea83202b41
rafael.ferreira <rafah.26ferreira@hotmail.com>
2020-06-14 17:20:25 -0300

    tela login

---
+++ UIKIT Swift__2020/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  UIKIT Swift__2020
+//
+//  Created by Rafael on 6/14/20.
+//  Copyright © 2020 Agesandro Scarpioni. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var senhaTxt: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
