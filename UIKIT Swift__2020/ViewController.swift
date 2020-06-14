1ac8fe00758a3d797222d3cd0bb0c0312fc7db0d
rafael.ferreira <rafah.26ferreira@hotmail.com>
2020-06-14 17:11:55 -0300

    Navigation controll

--- UIKIT Swift__2020/ViewController.swift
+++ UIKIT Swift__2020/ViewController.swift
@@ -11,47 +11,6 @@ import UIKit
 class ViewController: UIViewController {
     
     
-    @IBOutlet weak var lblIdade: UILabel!
-    @IBOutlet weak var lblPAS: UILabel!
-    @IBOutlet weak var lblPAD: UILabel!
-    @IBOutlet weak var lblSMS: UILabel!
-    @IBOutlet weak var clasRisco: UISegmentedControl!
-    @IBOutlet weak var meuStepper: UIStepper!
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    
-    
-    
-    @IBAction func sliderPasValueChanged(_ sender: UISlider) {
-        lblPAS.text = "\(Int(sender.value))"
-    }
-    
-    @IBAction func stepperValueChanged(_ sender: Any) {
-        lblIdade.text = "\(Int(meuStepper.value))"
-    }
-    
-    
-    
-    @IBAction func indexChange(_ sender: Any) {
-        switch (clasRisco.selectedSegmentIndex) {
-        case 0:
-            print("Você clicou no botão 1")
-        case 1:
-            print("Você clicou no botão 2")
-        case 2:
-            print("Você clicou no botão 3")
-        case 3:
-            print("Você clicou em \(clasRisco.titleForSegment(at: clasRisco.selectedSegmentIndex)!)")
-        default:
-            break
-        }
-    }
-    
     
 }
 
