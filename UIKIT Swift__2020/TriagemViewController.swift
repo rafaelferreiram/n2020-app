554869b0bd40629cdf9768e715c4dc4ef1cd99bc
rafael.ferreira <rafah.26ferreira@hotmail.com>
2020-06-14 18:08:24 -0300

    triagem Controller

---
+++ UIKIT Swift__2020/TriagemViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TriagemViewController.swift
+//  UIKIT Swift__2020
+//
+//  Created by Rafael on 6/14/20.
+//  Copyright © 2020 Agesandro Scarpioni. All rights reserved.
+//
+
+import UIKit
+
+class TriagemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
