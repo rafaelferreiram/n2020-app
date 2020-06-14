95968b364578f0b42aa992389b210d6c564813f2
Lucas Nogueira <lucas_snogueira@hotmail.com>
2020-06-14 20:43:14 -0300

    add new button component (rounded)

---
+++ UIKIT Swift__2020/MenuViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MenuViewController.swift
+//  UIKIT Swift__2020
+//
+//  Created by Lucas Nogueira on 14/06/20.
+//  Copyright © 2020 Agesandro Scarpioni. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    @IBOutlet weak var novaTriagemButton: UIButton!
+
+
+}
