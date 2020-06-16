f4edaf0687354b810affd19298aa8438cca51d95
rafael.ferreira <rafah.26ferreira@hotmail.com>
2020-06-15 22:11:42 -0300

    gerando qr code de triagem

--- UIKIT Swift__2020/MinhasTriagensViewController.swift
+++ UIKIT Swift__2020/MinhasTriagensViewController.swift
@@ -11,30 +11,10 @@ import UIKit
 class MinhasTriagensViewController: UIViewController {
 
     @IBOutlet weak var img: UIImageView!
-    
+    var traigem = TriagemViewController()
     override func viewDidLoad() {
         super.viewDidLoad()
-        img.image = generateQRCode(from: "Testing QR Code!")
-    }
-    
-    func generateQRCode(from string:String) -> UIImage? {
-        
-        let data = string.data(using: String.Encoding.ascii)
-        
-        if let filter = CIFilter(name: "CIQRCodeGenerator"){
-            
-            filter.setValue(data, forKey: "inputMessage")
-            
-            let transform = CGAffineTransform(scaleX: 3, y: 3)
-            
-            if let output = filter.outputImage?.transformed(by: transform) {
-                return UIImage(ciImage: output)
-            }
-            
-        }
-        
-        return nil
-        
+        img.image = traigem.getQrCode()
     }
 
 }
--- UIKIT Swift__2020/TriagemViewController.swift
+++ UIKIT Swift__2020/TriagemViewController.swift
@@ -13,5 +13,28 @@ class TriagemViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+   
+    func getQrCode () ->UIImage?{
+        return generateQRCode(from: "Sua senha para a triagem -> 78")
+    }
+     
+    func generateQRCode(from string:String) -> UIImage? {
+           
+           let data = string.data(using: String.Encoding.ascii)
+           
+           if let filter = CIFilter(name: "CIQRCodeGenerator"){
+               
+               filter.setValue(data, forKey: "inputMessage")
+               
+               let transform = CGAffineTransform(scaleX: 3, y: 3)
+               
+               if let output = filter.outputImage?.transformed(by: transform) {
+                   return UIImage(ciImage: output)
+               }
+               
+           }
+           
+           return nil
+           
+       }
 }
